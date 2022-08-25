44307e8540e9d4d1443d1e95e49c0fceefdce287
lhikarul <711hikaru@gmail.com>
2022-08-25 20:47:11 +0800

    打包 css 至 bundle.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("hello");
+import "./css/bootstrap.css";
+import "./js/slick/slick.css";
+import "./js/slick/slick-theme.css";
+import "./css/style.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "/src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "js/bundle.js",
     path: path.resolve(__dirname, "./dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[name].[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
