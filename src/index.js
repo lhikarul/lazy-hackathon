9994d3e89acb36f84ec8ba2bdc16f0e9dd7385c3
lhikarul <711hikaru@gmail.com>
2022-08-25 21:39:06 +0800

    chore: 引入 js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "./css/bootstrap.css";
 import "./js/slick/slick.css";
 import "./js/slick/slick-theme.css";
 import "./css/style.css";
+
+import "jquery";
+import "slick-carousel";
+import "./js/index.js";
