8ac73dd877f2435baa11574f3c0b754bb863a0b1
lhikarul <711hikaru@gmail.com>
2022-08-27 09:15:22 +0800

    image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,34 @@ module.exports = {
       },
       {
         test: /.(png|svg|jpg|jpeg|gif|webp)$/i,
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: "asset",
         parser: {
           dataUrlCondition: {
